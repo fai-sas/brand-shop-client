99c916833bedfc682f2264f02af3b38fd9f62726
fai-sas <craftedbyfaisal@gmail.com>
2023-10-19 19:37:30 +0600

    single product detailes page created and shown product details

--- src/Pages/ProductDetailsPage.jsx
+++ src/Pages/ProductDetailsPage.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 import { useEffect, useState } from 'react'
-import { useLoaderData, useParams } from 'react-router-dom'
+import { Link, useLoaderData, useParams } from 'react-router-dom'
 
 const ProductDetailsPage = () => {
   const [singleProduct, setSingleProduct] = useState([])
@@ -11,13 +11,48 @@ const ProductDetailsPage = () => {
   useEffect(() => {
     const findProduct = data?.find((product) => product._id === id)
     setSingleProduct(findProduct)
-    console.log(singleProduct)
   }, [id, data])
 
   return (
     <>
-      <div className='container p-8 mx-auto text-4xl font-bold'>
-        ProductDetailsPage : {singleProduct.name}
+      <div className='bg-white border rounded-xl shadow-sm sm:flex dark:bg-gray-800 dark:border-gray-700 dark:shadow-slate-700/[.7]'>
+        <div className='flex-shrink-0 relative w-full rounded-t-xl overflow-hidden pt-[40%] sm:rounded-l-xl sm:max-w-[15rem] md:rounded-tr-none md:max-w-xs'>
+          <img
+            src={singleProduct.image}
+            alt={singleProduct.name}
+            className='absolute top-0 left-0 object-cover w-full h-full'
+          />
+        </div>
+        <div className='flex flex-wrap'>
+          <div className='flex flex-col h-full p-4 sm:p-7'>
+            <h3 className='text-lg font-bold text-gray-800 dark:text-white'>
+              {singleProduct.name}
+            </h3>
+            <p className='mt-1 text-gray-800 dark:text-gray-400'>
+              {singleProduct.description}
+            </p>
+            <div className='mt-5 sm:mt-auto'>
+              <Link to='#'>
+                <button
+                  type='button'
+                  className='inline-flex items-center justify-center gap-2 px-4 py-3 text-sm font-semibold text-white transition-all bg-blue-500 border border-transparent rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800'
+                >
+                  <svg
+                    className='w-3.5 h-3.5'
+                    xmlns='http://www.w3.org/2000/svg'
+                    width='16'
+                    height='16'
+                    fill='currentColor'
+                    viewBox='0 0 16 16'
+                  >
+                    <path d='M5.071 1.243a.5.5 0 0 1 .858.514L3.383 6h9.234L10.07 1.757a.5.5 0 1 1 .858-.514L13.783 6H15.5a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.5.5H15v5a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V9H.5a.5.5 0 0 1-.5-.5v-2A.5.5 0 0 1 .5 6h1.717L5.07 1.243zM3.5 10.5a.5.5 0 1 0-1 0v3a.5.5 0 0 0 1 0v-3zm2.5 0a.5.5 0 1 0-1 0v3a.5.5 0 0 0 1 0v-3zm2.5 0a.5.5 0 1 0-1 0v3a.5.5 0 0 0 1 0v-3zm2.5 0a.5.5 0 1 0-1 0v3a.5.5 0 0 0 1 0v-3zm2.5 0a.5.5 0 1 0-1 0v3a.5.5 0 0 0 1 0v-3z' />
+                  </svg>
+                  Add to cart
+                </button>
+              </Link>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   )
